7153b759022d3c8e0646df5c16b2169603b6032c
riceriley59 <64724036+riceriley59@users.noreply.github.com>
2022-07-31 17:48:34 -0700

    added some client-side styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
+import { useState, useEffect} from 'react';
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [popupActive, setPopupActive] = useState(false);
+  const [newTodo, setNewTodo] = useState("");
+
   return (
     <div className="App">
       <h1>Welcome</h1>
